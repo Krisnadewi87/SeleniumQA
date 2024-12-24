1d6648c39ff0f03b1f7cda5eb64a325edbf98fb6
Ari Krisnadewi <kadekarikrisnadewi@gmail.com>
2024-12-24 12:37:11 +0800

    sesi 9 demo saucedemologintest

--- sauceDemoLoginTest.js
+++ sauceDemoLoginTest.js
@@ -7,7 +7,8 @@ async function sauceDemoLoginTest() {
     
     try {
         /// Implicit Wait
-        await driver.manage().setTimeouts({ implicit: 2000 });
+        // await driver.manage().setTimeouts({ implicit: 5000 });
+    
         await driver.get("https://www.saucedemo.com/");
         // Enter the username and password
         await driver.findElement(By.id('user-name')).sendKeys('standard_user');
@@ -18,8 +19,8 @@ async function sauceDemoLoginTest() {
         
         // Verify the web title: "Swag Labs" displayed on the Dashboard
         let titleText = await driver.findElement(By.xpath("//div[@class='app_logo']")).getText();
-        // console.log(titleText);
         assert.strictEqual(titleText.includes('Swag Labs'), true, "Title does not include 'Swag Labs'");
+        console.log(titleText);
         /// Explicit Wait
         // let titleText = await driver.findElement(By.xpath("//div[@class='app_logo']"));
         // await driver.wait(until.elementIsVisible(titleText), 2000);
