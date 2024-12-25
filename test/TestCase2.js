2758a1f39df345a33cf443241a4de03f5e048ee6
Ari Krisnadewi <kadekarikrisnadewi@gmail.com>
2024-12-25 20:36:19 +0800

    new update

--- test/TestCase2.js
+++ test/TestCase2.js
@@ -1,39 +1,40 @@
 const { Builder } = require('selenium-webdriver');
-const LoginPage = require('./WebComponent/LoginPage');
-// const DashboardPage = require('./WebComponent/DashboardPage');
+const LoginPage = require('../WebComponent/LoginPage');
 const assert = require('assert');
+require('dotenv').config();
 
-describe('TestCase 2', function () {
+const baseUrl = process.env.BASE_URL;
+
+
+describe('TestCase2 [login] #Smoke', function () {
     this.timeout (40000);
     let driver;
 
     //Run setiap mulai test, satu kali saja paling awal
     before (async function (){
-        driver = await new Builder(). forBrowser( 'chrome').build();
+        driver = await new Builder(). forBrowser('chrome').build();
     });
 
     //Test Suite dimulai dengan apa, setiap melakukan test
     beforeEach(async function () {
     const loginPage = new LoginPage(driver);
-    await loginPage.navigate();
+    await loginPage.navigate(baseUrl);
     await loginPage. login('haha', 'hihi');
     });
 
     //Assertion atau validasi
     it('Error message appears for invalid credentials', async function () {
         const loginPage = new LoginPage(driver);
-        const errorMessage = await loginPage.getErrorMessage);
+        const errorMessage = await loginPage.getErrorMessage();
+        assert.strictEqual(errorMessage, 'Epic sadface: Username and password, 'Expected error message does not match')
     })
 
         
     
-    // {
+    {
 
-    //     
-    //     assert.strictEqual(errorMessage, 'Epic sadface: Username and password, 'Expected error message does not match')
-    // });
+    });
 
-    // after(async function (){
-    //     await driver.quit();
-    // });
-});
+    after(async function (){
+        await driver.quit();
+    };
